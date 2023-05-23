052ec1e249ce7cddc3d77c7189f4f87ddcda9d56
phucquyet1202 <phucquyet1222003@gmail.com>
2023-05-23 10:34:33 +0700

    feat: signIn/signUp Refs: FF1-4

--- src/app/pages/auth/sign-in/sign-in.component.ts
+++ src/app/pages/auth/sign-in/sign-in.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sign-in',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./sign-in.component.css']
 })
 export class SignInComponent {
-
+  constructor(private router: Router) { }
+  routers() {
+    this.router.navigate(['/auth/signUp']);
+  }
 }
