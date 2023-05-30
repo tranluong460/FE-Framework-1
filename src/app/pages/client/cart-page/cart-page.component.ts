04431f77b584273269d69604f963ea7244404ebd
Alone <tranluong460@gmail.com>
2023-05-30 21:23:24 +0700

    feat: reopen cart

--- src/app/pages/client/cart-page/cart-page.component.ts
+++ src/app/pages/client/cart-page/cart-page.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-cart-page',
   templateUrl: './cart-page.component.html',
-  styleUrls: ['./cart-page.component.css']
+  styleUrls: ['./cart-page.component.css'],
 })
 export class CartPageComponent {
+  items: any[];
 
+  constructor() {
+    this.items = Array(2).fill(0);
+  }
 }
