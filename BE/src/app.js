d46b8a19dc3056a722f7579219317117062fd600
phucquyet1202 <phucquyet1222003@gmail.com>
2023-05-31 17:42:07 +0700

    feat: services product ref: FF1-21

--- BE/src/app.js
+++ BE/src/app.js
@@ -13,11 +13,11 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
-// app.use('/product') // Router quản lý sản phẩm
+app.use('/product', productRouter) // Router quản lý sản phẩm
 // app.use('/category') // Router quản lý danh mục
 // app.use('/order') // Router quản lý đơn hàng
 // app.use('/comment') // Router bình luận
-app.use('/', authRouter)
+// app.use('/', authRouter)
 // app.use('/') // Router quên mật khẩu
 
 mongoose.connect(process.env.URI)
