2f90a537af47d0afe297a3ba6fc4304bdd3c151e
phucquyet1202 <phucquyet1222003@gmail.com>
2023-06-01 23:49:34 +0700

    reopen be product

--- BE/src/controller/product.js
+++ BE/src/controller/product.js
@@ -1,69 +1,48 @@
 import Product from "../models/product";
-import { productSchema } from "../validate/product";
+import { productSchema } from '../validate/product'
 
-export const getAll = async (req, res) => {
+const getAll = async (req, res) => {
     try {
-        const data = await Product.find()
+        const data = await Product.find().populate('brand');
+
         if (data.length === 0) {
-            return res.status(404).json({ message: 'khong tim thay san pham' })
+            return res.status(200).json({
+                message: "Không có dữ liệu"
+            })
         }
-        return res.status(200).json({
-            message: data
-        })
-    } catch (error) {
-        return res.status(404).json({ message: error.message });
+        return res.json(data)
     }
-
-}
-export const get = async (req, res) => {
-    try {
-        const data = await Product.findById(req.params.id).populate('brand')
-        if (data.length === 0) {
-            return res.status(404).json({ message: 'khong tim thay san pham' })
-        }
-        return res.status(200).json({
-            message: data
-        })
-    } catch (error) {
-        return res.status(404).json({ message: error.message });
-    }
-
-}
-export const createProduct = async (req, res) => {
-    try {
-            const { error } = productSchema.validate(req.body, { abortEarly: false });
-            if (error) {
-                const errors = error.details.map((err) => err.message);
-                return res.status(400).json({
-                    message: errors,
-                });
-            }
-        const data = await Product.create(req.body)
-        if (data.length === 0) {
-            return res.status(404).json({ message: 'khong them duoc san pham' })
-        }
-        return res.status(200).json({
-            message: data
-        })
-    } catch (error) {
-        return res.status(404).json({ message: error.message });
-    }
-
-}
-export const deleteProduct = async (req, res) => {
-    try {
-        const data = await Product.findOneAndDelete({ _id: req.params.id })
-        return res.status(200).json({
-            message: 'xoa danh muc thanh cong',
-            data
-        })
-    } catch (error) {
+    catch (err) {
         return res.status(404).json({
-            message: error.message
-        })
+            message: err
+        });
     }
 }
-export const updateProduct = async (req, res) => {
+
+const getOne = async (req, res) => {
+    try {
+        const data = await Product.findById(req.params.id).populate('brand').populate({
+            path: 'comments',
+            populate: {
+                path: 'user', select: 'name'
+            }
+        });
+
+        if (data.length === 0) {
+            return res.status(200).json({
+                message: "Không có dữ liệu"
+            })
+        }
+        return res.json(data)
+    }
+    catch (err) {
+        return res.status(404).json({
+            message: err
+        });
+    }
+}
+
+const create = async (req, res) => {
     try {
         const { error } = productSchema.validate(req.body, { abortEarly: false });
         if (error) {
@@ -72,20 +51,72 @@ export const updateProduct = async (req, res) => {
                 message: errors,
             });
         }
+
+        const product = await Product.create(req.body);
+        return res.status(200).json({
+            message: 'Thêm sản phẩm thành công',
+            product
+        })
+    }
+    catch (err) {
+        return res.status(404).json({
+            message: err
+        });
+    }
+}
+
+const edit = async (req, res) => {
+    try {
+        const { error } = productSchema.validate(req.body, { abortEarly: false });
+        if (error) {
+            const errors = error.details.map((err) => err.message);
+            return res.status(400).json({
+                message: errors,
+            });
+        }
+
         const data = await Product.findOneAndUpdate(
             { _id: req.params.id },
             req.body,
             { new: true }
         );
+
         if (data.length === 0) {
-            return res.status(404).json({ message: 'khong cap nhat san pham' })
+            return res.status(200).json({
+                message: "Cập nhật dữ liệu không thành công"
+            })
         }
         return res.status(200).json({
-            message: 'cap nhat san pham thanh cong',
+            message: 'Cập nhật sản phẩm thành công',
             data
         })
-    } catch (error) {
-        return res.status(404).json({ message: error.message });
     }
+    catch (err) {
+        return res.status(404).json({
+            message: err
+        });
+    }
+}
 
+const del = async (req, res) => {
+    try {
+        const data = await Product.findOneAndDelete({ _id: req.params.id });
+
+        return res.json({
+            message: "Xóa dữ liệu thành công"
+        })
+    }
+    catch (err) {
+        return res.status(404).json({
+            message: err
+        });
+    }
+}
+
+export {
+    getAll,
+    getOne,
+    edit,
+    create,
+    del
 }
--- BE/src/router/product.js
+++ BE/src/router/product.js
@@ -1,9 +1,17 @@
-import express from "express"
-import { createProduct, deleteProduct, get, getAll, updateProduct } from "../controller/product"
+import express from "express";
+import { create, del, edit, getAll, getOne } from "../controller/product";
+import { checkPermission } from "../middleware/checkPermission";
+
 const router = express.Router()
-router.get('/', getAll)
-router.get('/:id', get)
-router.post('/add', createProduct)
-router.delete('/:id', deleteProduct)
-router.put('/:id', updateProduct)
+
+router.get("/", getAll)
+
+router.get("/:id", getOne)
+
+router.post("/", checkPermission, create)
+
+router.put("/:id", checkPermission, edit)
+
+router.delete('/:id', checkPermission, del)
+
 export default router
