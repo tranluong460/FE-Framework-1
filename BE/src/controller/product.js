69c7440b811f01a6ccd8827e76dc19365d365efe
phucquyet1202 <phucquyet1222003@gmail.com>
2023-06-01 23:44:28 +0700

    feat: up BE product refs: FF1-16

--- BE/src/controller/product.js
+++ BE/src/controller/product.js
@@ -1,4 +1,5 @@
 import Product from "../models/product";
+import { productSchema } from "../validate/product";
 
 export const getAll = async (req, res) => {
     try {
@@ -16,7 +17,7 @@ export const getAll = async (req, res) => {
 }
 export const get = async (req, res) => {
     try {
-        const data = await Product.findById(req.params.id)
+        const data = await Product.findById(req.params.id).populate('brand')
         if (data.length === 0) {
             return res.status(404).json({ message: 'khong tim thay san pham' })
         }
@@ -30,6 +31,13 @@ export const get = async (req, res) => {
 }
 export const createProduct = async (req, res) => {
     try {
+            const { error } = productSchema.validate(req.body, { abortEarly: false });
+            if (error) {
+                const errors = error.details.map((err) => err.message);
+                return res.status(400).json({
+                    message: errors,
+                });
+            }
         const data = await Product.create(req.body)
         if (data.length === 0) {
             return res.status(404).json({ message: 'khong them duoc san pham' })
@@ -44,7 +52,7 @@ export const createProduct = async (req, res) => {
 }
 export const deleteProduct = async (req, res) => {
     try {
-        const data = await Product.findByIdAndDelete({ _id: req.params.id })
+        const data = await Product.findOneAndDelete({ _id: req.params.id })
         return res.status(200).json({
             message: 'xoa danh muc thanh cong',
             data
@@ -57,12 +65,24 @@ export const deleteProduct = async (req, res) => {
 }
 export const updateProduct = async (req, res) => {
     try {
-        const data = await Product.findByIdAndUpdate({ _id: req.params.id }, req.body, { new: true })
+        const { error } = productSchema.validate(req.body, { abortEarly: false });
+        if (error) {
+            const errors = error.details.map((err) => err.message);
+            return res.status(400).json({
+                message: errors,
+            });
+        }
+        const data = await Product.findOneAndUpdate(
+            { _id: req.params.id },
+            req.body,
+            { new: true }
+        );
         if (data.length === 0) {
-            return res.status(404).json({ message: 'khong them san pham' })
+            return res.status(404).json({ message: 'khong cap nhat san pham' })
         }
         return res.status(200).json({
-            message: data
+            message: 'cap nhat san pham thanh cong',
+            data
         })
     } catch (error) {
         return res.status(404).json({ message: error.message });
