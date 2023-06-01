6bbee72634d207cce4ffb5ec08bdf4cab5b26e83
Alone <tranluong460@gmail.com>
2023-06-01 23:51:54 +0700

    up

--- BE/src/app.js
+++ BE/src/app.js
@@ -15,7 +15,7 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
-// app.use('/product', productRouter)
+app.use('/product', productRouter)
 app.use('/', categoriRouter)
 app.use('/order', orderRouter)
 // app.use('/comment') // Router bình luận
