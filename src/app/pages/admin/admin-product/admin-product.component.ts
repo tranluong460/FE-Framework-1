ff77ebb9a4b5430338722d2cf66ef7b551da63f4
phucquyet1202 <phucquyet1222003@gmail.com>
2023-06-06 17:50:52 +0700

    feat: servixe admin product refs: FF1-32

--- src/app/pages/admin/admin-product/admin-product.component.ts
+++ src/app/pages/admin/admin-product/admin-product.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ProductsService } from 'src/app/services/products/products.service';
 
 @Component({
   selector: 'app-admin-product',
@@ -7,10 +8,21 @@ import { Component } from '@angular/core';
 })
 export class AdminProductComponent {
   products: any[];
-
+  product: any
   p: number = 1;
 
-  constructor() {
+  constructor(private productsService: ProductsService) {
     this.products = Array(14).fill(0);
+    this.productsService.getAllProducts().subscribe(product => {
+      this.products = product.data
+      console.log(this.products);
+    })
+  }
+  getItemById(id: any) {
+    this.productsService.getProduct(id).subscribe(pro => {
+      console.log(pro.data);
+      this.product = pro.data;
+      console.log(this.product);
+    })
   }
 }
