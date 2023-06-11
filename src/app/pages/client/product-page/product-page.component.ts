ae4d74a546d432fa99dd4e418edd99320d6608cc
Alone <tranluong460@gmail.com>
2023-06-11 13:02:02 +0700

    up product page

--- src/app/pages/client/product-page/product-page.component.ts
+++ src/app/pages/client/product-page/product-page.component.ts
@@ -1,8 +1,34 @@
 import { Component } from '@angular/core';
-
+import { CartService } from 'src/app/services/cart/cart.service';
+import { ProductsService } from 'src/app/services/products/products.service';
 @Component({
   selector: 'app-product-page',
   templateUrl: './product-page.component.html',
   styleUrls: ['./product-page.component.css'],
 })
-export class ProductPageComponent {}
+export class ProductPageComponent {
+  items: any;
+  isLoading: any = true;
+
+  formatCurrency(amount: number): string {
+    const formattedAmount = amount.toLocaleString('en-US');
+    return `${formattedAmount}₫`;
+  }
+
+  constructor(
+    private productsService: ProductsService,
+    private cartService: CartService
+  ) {
+    this.items = Array(6).fill(0);
+    this.productsService.getAllProducts().subscribe((data) => {
+      this.items = data.data;
+    });
+    setTimeout(() => {
+      this.isLoading = false;
+    }, 1000);
+  }
+
+  addToCart(product: any) {
+    this.cartService.addToCart(product);
+  }
+}
